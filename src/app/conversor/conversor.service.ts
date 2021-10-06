b732b0d505e10bd3943f075e4b3a0567466ad26b
Elis Marcela <82593291+develis@users.noreply.github.com>
2021-10-06 13:46:58 -0300

    initial commit

---
+++ src/app/conversor/conversor.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+import { Unidades } from "./unidades";
+
+@Injectable()
+export class ConversorService {
+    constructor() { }
+
+    getUnidades() {
+        return Unidades.map((unidade) => unidade.unidade)
+    }
+
+    isNumber(n: any) {
+        return !isNaN(parseFloat(n)) && isFinite(n)
+    }
+
+    formatarUnidade(unidadeString: string) {
+        const partes = unidadeString.split(' ')
+        const valor = parseFloat(partes[0])
+
+        if (!this.isNumber(valor)) {
+            return null // acho que pode ser false também
+        }
+        const unidade = Unidades.filter((unidade) => partes[1].toLowerCase() === unidade.unidade)
+
+        if (!unidade[0]) {
+            return null
+        }
+
+        return {
+            val: valor,
+            uni: unidade[0].unidade
+        }
+    }
+
+    converterParaUnidade(valor: number, recebido: string, tratado: string) {
+        var conversao;
+        conversao = valor / Unidades.find((unidade) => unidade.unidade === recebido).paraMetros
+        conversao *= Unidades.find((unidade) => unidade.unidade === tratado).paraMetros
+        return parseFloat(conversao.toFixed(6));
+    }
+
+}
